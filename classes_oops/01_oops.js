b678e567b67060d44e0b7a7f51e3116e266ae407
125megha <134535703+125megha@users.noreply.github.com>
2025-07-07 17:06:34 +0000

    change

---
+++ classes_oops/01_oops.js
@@ -0,0 +1,38 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+//with the help this and new you can the function act as class 
+// with the new keyword you can make new instance otherwise it will overwrite the same value
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
